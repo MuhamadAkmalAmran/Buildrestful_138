87f135aa924ace19ee532d8ecf76f00bd5144506
akmal <akmal@DESKTOP-QEPVMLM>
2022-12-05 00:36:07 +0700

    Create Package Modal n Product Java Class

--- src/main/java/Modal/Product.java
+++ src/main/java/Modal/Product.java
@@ -2,17 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.pws.a.Buildrestful;
-
-import org.springframework.web.bind.annotation.RestController;
+package Modal;
 
 /**
  *
  * @author akmal
  */
-//Digunakan untuk mendefinisikan RESTful 
-@RestController
-public class ProductController {
-    
+public class Product {
     
 }
