9ac4c42037e8098ac497f9f816477ee85283b997
akmal <akmal@DESKTOP-QEPVMLM>
2022-12-04 18:13:49 +0700

    Create Project

---
+++ src/main/java/com/pws/a/Buildrestful/BuildrestfulApplication.java
@@ -0,0 +1,13 @@
+package com.pws.a.Buildrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildrestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildrestfulApplication.class, args);
+	}
+
+}
